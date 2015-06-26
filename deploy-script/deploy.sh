ef59395093b650f7022cf5fd6f5dfd9f8d03b78c
Carlos <cmoro@deusto.es>
2015-06-26 11:55:19 +0200

    Fix deploy script. Add version checking and error handling to deploy.sh. Complete Deployment description.

--- deploy-script/deploy.sh
+++ deploy-script/deploy.sh
@@ -3,21 +3,43 @@
 set -e
 NAME=app
 USER=ubuntu
-APPLICATION_DIRECTORY=/opt/app/Bolala-code
- 
+APPLICATION_DIRECTORY=/opt/app/ball-pubapi
+
+check_upstart_service(){
+    status $1 | grep -q "^$1 start" > /dev/null
+    return $?
+}
+
 start() {
+
     echo "Deploying $NAME..."
-    echo "Stopping service"
-    sudo service $NAME stop
+
     echo "Changing directory"
     cd $APPLICATION_DIRECTORY
-    echo "Getting release"
-    sudo -u $USER git pull
-    echo "Packagin application"
-    activator clean stage
-    echo "Starting service"
-    sudo service $NAME start
-    RETVAL=$?
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
+    echo "Local version is: $LOCAL"
+    echo "Remote version is: $REMOTE"
+
+    if [ $LOCAL = $REMOTE ]; then
+      echo "Release is up to date!"
+    else
+
+      echo "Getting release... $REMOTE"
+
+      if check_upstart_service app; 
+      then 
+        echo "Stopping Service" 
+        sudo service $NAME stop;
+      fi
+
+      sudo -u $USER git pull
+      echo "Packagin application"
+      activator clean stage
+      echo "Starting service"
+      sudo service $NAME start
+      RETVAL=$?
+    fi
 }
 
 case "$1" in
